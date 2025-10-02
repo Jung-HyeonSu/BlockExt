51ab962649ff39ae3ba13ccf3c7672b1315111cb
Jung-HyeonSu <gustn05233@gmail.com>
2025-10-02 22:13:25 +0900

    refactor: 확장자 엔티티 레포지토리 구현 및 분리

---
+++ backend/src/main/java/com/flow/blockext/repository/CustomExtensionRepository.java
@@ -0,0 +1,10 @@
+package com.flow.blockext.repository;
+
+import com.flow.blockext.entity.CustomExtension;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CustomExtensionRepository extends JpaRepository<CustomExtension, Long> {
+    Optional<CustomExtension> findByName(String name);
+}
---
+++ backend/src/main/java/com/flow/blockext/repository/FixedExtensionRepository.java
@@ -0,0 +1,10 @@
+package com.flow.blockext.repository;
+
+import com.flow.blockext.entity.FixedExtension;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface FixedExtensionRepository extends JpaRepository<FixedExtension, Long> {
+    Optional<FixedExtension> findByName(String name);
+}
