a6bf2f22355c0b39d1d84daa57db6bfdeda3b0c1
Hyeonsu <92723729+Jung-HyeonSu@users.noreply.github.com>
2025-10-02 16:42:33 +0900

    chore: Spring Boot 프로젝트 초기 생성

---
+++ backend/src/main/java/com/flow/blockext/BlockExtApplication.java
@@ -0,0 +1,13 @@
+package com.flow.blockext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockExtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockExtApplication.class, args);
+	}
+
+}
